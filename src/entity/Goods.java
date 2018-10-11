9588a6f20cfa0b99d49b16ff626eaf7f54fd70fe
zjwzjwzjwzjw <18937076190@189.cn>
2018-10-11 09:20:42 +0800

    Initial commit

---
+++ src/entity/Goods.java
@@ -0,0 +1,75 @@
+package entity;
+
+/**
+ * Created by user on 2018/9/28.
+ */
+public class Goods {
+    private Integer id;
+    private String name;
+    private String type;
+    private Integer count;
+    private Double price;
+    private Double sumPrice;
+    public Goods() {
+    }
+
+    public Double getSumPrice() {
+        return sumPrice;
+    }
+
+    public void setSumPrice(Double sumPrice) {
+        this.sumPrice = sumPrice;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", count=" + count +
+                ", price=" + price +
+                ", sumPrice=" + sumPrice +
+                '}';
+    }
+}
